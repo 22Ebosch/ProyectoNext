11338955ccfe5748401143680cbbd20553560c76
Eudald <149712038+22Ebosch@users.noreply.github.com>
2024-05-18 14:45:56 +0200

    Estilos

--- src/components/kanban/kanban.tsx
+++ src/components/kanban/kanban.tsx
@@ -1,4 +1,4 @@
-
+// Kanban.tsx
 "use client";
 import { getTareasPendientes, getTareasEnProgreso, getTareasFinalizadas } from "@/lib/data";
 import { useEffect, useState } from "react";
@@ -9,7 +9,7 @@ const Kanban = ({ onUpdate, onButtonClick, tareasPendientes, tareasEnProgreso, t
       return (
         <>
           <h1 className='text-center m-4'>Bienvenido a mi sitio web</h1>
-          <div className="flex justify-around p-4">
+          <div className="flex justify-around p-4 items-start">
             <div className="w-1/4 bg-red-200 p-2 rounded">
               <h2 className="font-bold mb-2">Pendientes</h2>
               {tareasPendientes.map((tarea) => (
--- src/components/tareas/tarea.tsx
+++ src/components/tareas/tarea.tsx
@@ -10,7 +10,7 @@ const Tarea = ({ tarea, onUpdate }) => {
 
   return (
     <div id={tarea.id} className="bg-white p-2 rounded mb-2 text-black relative group">
-      <div className='text-right absolute right-0 top-0 invisible group-hover:visible'>
+      <div className='text-right absolute right-0 top-0 invisible group-hover:visible mr-3'>
         <a onClick={handleDelete} className="text-red-500 underline cursor-pointer">Eliminar</a>
       </div>
       <h1 className="text-2xl">{tarea.titulo}</h1>
